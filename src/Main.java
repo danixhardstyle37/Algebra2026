fac18a8172c6f84474ee47a2274ab1a4101bf78a
Dario <85134549+danixhardstyle37@users.noreply.github.com>
2026-05-04 20:21:53 +0200

    Math pow i sqrt metode

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,26 @@ public class Main {
         LocalDateTime dt = LocalDateTime.now();
         System.out.println(dt);
         System.out.println(dt);
+        System.out.println(dt.getDayOfWeek());
+        System.out.println(dt.getMonth().plus(1));
+
+        double a = 4;
+        double b = 3;
+        double c = 16;
+
+        double kvadratA = Math.pow(a,2);
+        double kvadratC = Math.pow(c,2);
+
+        System.out.println("Kvadrat broja " + a + ": " + kvadratA);
+        System.out.println("Kvadrat broja " + c + ": " + kvadratC);
+
+        double korijenC = Math.sqrt(c);
+
+        System.out.println("Korijen broja " + c +": " + korijenC);
+
+        double bNaTrecu = Math.pow(b,3);
+
+        System.out.println(b + " na treću potenciju: " + bNaTrecu);
+
     }
 }
